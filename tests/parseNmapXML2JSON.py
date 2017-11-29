0c4aad00c74f776d36442f0b673a302e3956e7be
Epzylon <lord.epzylon@gmail.com>
2017-11-29 15:25:49 -0300

    Folder reorganization

---
+++ tests/parseNmapXML2JSON.py
@@ -0,0 +1,12 @@
+#!env python
+import sys
+from nmap2json import NmapScanToJson as parseNmap
+
+filename = str(sys.argv[1])
+
+try:
+	output = parseNmap(filename)
+except:
+	print("Can't parse the fie: " + filename)
+
+print(output.get_json())
---
+++ tests/uploadXML.py
@@ -0,0 +1,8 @@
+#!env python
+#Sample script to parse and upload an nmap xml
+import sys
+from mdbdriver import uploadScan
+
+upload = uploadScan(sys.argv[1])
+upload.test_db()
+upload.upload()
