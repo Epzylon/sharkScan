c950e4fe295725e46b32ad031b5ddfef7732c697
NetscureTeam <lord.epzylon@gmail.com>
2017-12-10 09:30:50 -0300

    selected scan type as property

--- sharkPlugins/genericObjects.py
+++ sharkPlugins/genericObjects.py
@@ -30,6 +30,8 @@ class Plugin(object):
         
         #Execution parameters
         self.binary = binary
+        self.command = self.binary
+        self.args = ""
         self.basic_args = None
         self.target_parameter = None
         self.output_parameter = None
@@ -47,10 +49,20 @@ class Plugin(object):
         self.supported_types.append(basic)
         
         #Index for the supported_types array
-        self.selected_type = 0
+        self._selected_type = 0
+
+
+        
+    @property
+    def selected_type(self):
+        return(self._selected_type)
+    
+    @selected_type.setter
+    def selected_type(self,value):
+        self._selected_type = value
+        self._configure_selected_scan()
     
     def _configure_selected_scan(self):
-        
         #Check if sudo is required
         if self.supported_types[self.selected_type].require_privileges:
             self.command = self.supported_types[self.selected_type].privileges_prefix + self.SEPARATOR + self.command
@@ -73,13 +85,7 @@ class Plugin(object):
         self.args = self.args + self.SEPARATOR + self.output_path
         
 
-    def set_target(self, target):
-        self.command = self.binary
-        self.args = ""
-        
-        self._configure_selected_scan()
-        self._configure_basics()
-            
+    def set_target(self, target):                    
         #Check if there are target parameter to be set
         if self.target_parameter != None:
             self.args = self.args + self.target_parameter
@@ -87,7 +93,8 @@ class Plugin(object):
         self.args = self.args + self.SEPARATOR + target
     
     def run(self):
-        system(self.binary + self.SEPARATOR + self.args)
+        self._configure_basics()
+        system(self.command + self.SEPARATOR + self.args)
             
         
         
